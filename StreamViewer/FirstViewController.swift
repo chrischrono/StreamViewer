2f5902006f54717787953f2492ca7bc891634e0f
Christian <chris_chrono@yahoo.com>
2019-05-23 16:51:27 +0700

    Initial Commit

---
+++ StreamViewer/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  StreamViewer
+//
+//  Created by Christian on 23/05/19.
+//  Copyright © 2019 Christian. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StreamViewer/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  StreamViewer
+//
+//  Created by Christian on 23/05/19.
+//  Copyright © 2019 Christian. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
